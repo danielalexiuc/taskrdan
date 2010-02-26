87058d12197d4a439b2947e98d003507355453b9
Daniel Alexiuc <daniel@alexiuc.com>
2010-02-26 16:09:06 +1000

    alert when finished ordering

--- evently/tasks/recent/after.js
+++ evently/tasks/recent/after.js
@@ -1,4 +1,25 @@
 function() {
-    $("#sortable").sortable();
+    $("#sortable").sortable({
+        stop: function(event, ui) {
+            alert('Hey it worked.');
+//            function() {
+//                var li = $(this).parents("li");
+//                var task_id = li.attr("data-id");
+//                var app = $$(this).app;
+//                app.db.openDoc(task_id, {
+//                    success : function(doc) {
+//                        doc.danielOrder = 1;
+//                        app.db.saveDoc(doc, {
+//                            success : function() {
+//                                li.addClass("done");
+//                                li.slideUp("slow");
+//                            }
+//                        });
+//                    }
+//                });
+//                return false;
+//            }
+        }
+    });
     $("#sortable").disableSelection();
 }

4a7ad64db3444550acc147ccdcf292cf0b873605
Daniel Alexiuc <daniel@alexiuc.com>
2010-02-26 16:52:37 +1000

    remove alerts

--- evently/tasks/recent/after.js
+++ evently/tasks/recent/after.js
@@ -7,15 +7,10 @@ function() {
                     function(i) {
                         var li = $(this);
                         var task_id = li.attr("data-id");
-                        alert(task_id);
                         app.db.openDoc(task_id, {
                             success : function(doc) {
                                 doc.danielOrder = i;
-                                app.db.saveDoc(doc, {
-                                    success : function() {
-                                        alert("It worked!");
-                                    }
-                                });
+                                app.db.saveDoc(doc);
                             }
                         });
                     });

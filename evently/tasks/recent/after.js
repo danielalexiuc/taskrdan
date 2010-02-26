21c40e87b8fe653f3f9c83f3ee3fa8138328e7bf
Daniel Alexiuc <daniel@alexiuc.com>
2010-02-26 16:45:07 +1000

    second attempt at persisting

--- evently/tasks/recent/after.js
+++ evently/tasks/recent/after.js
@@ -1,15 +1,13 @@
 function() {
+    var app = $$(this).app;
+
     $("#sortable").sortable({
         stop: function(event, ui) {
-            alert('Start');
-
             $("#sortable").children().each(
                     function(i) {
-
                         var li = $(this);
                         var task_id = li.attr("data-id");
                         alert(task_id);
-                        var app = $$(this).app;
                         app.db.openDoc(task_id, {
                             success : function(doc) {
                                 doc.danielOrder = i;
